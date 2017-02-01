8bb09ae062e1a1ee8e8ea1873c8d860bb9995379
Patryk Mazurkiewicz <patrykmazurkiewicz@MacBook-Pro-Patryk.local>
2017-02-01 22:47:52 +0100

    calculations modification

--- modules/timeFormat.js
+++ modules/timeFormat.js
@@ -1,13 +1,15 @@
 function secondsToMinutes(seconds) {
-    var minutes = Math.floor(seconds / 60),
-        restSeconds = seconds - minutes * 60;
+    var restSeconds = seconds % 60,
+        minutes = (seconds - restSeconds) / 60;
+
     return minutes + ' min ' + restSeconds + ' sec';
 }
 
 function secondsToHours(seconds) {
-    var hours = Math.floor(seconds / 3600),
-        minutes = Math.floor((seconds - (hours * 3600)) / 60),
-        restSeconds = seconds - (hours * 3600) - (minutes * 60);
+    var restSeconds = seconds % 60,
+        minutes = ((seconds - restSeconds) % 3600) / 60,
+        hours = (seconds - (restSeconds + minutes*60)) / (60*60);
+
     return hours + ' h ' + minutes + ' min ' + restSeconds + ' sec';
 }
 
